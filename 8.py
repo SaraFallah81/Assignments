926816f9e67c38724e1c9724834c0a3437c4bf4d
SaraFallah81 <111195533+SaraFallah81@users.noreply.github.com>
2022-08-21 12:08:23 +0430

    Assignment 2
    Assignment 2.

---
+++ 8.py
@@ -0,0 +1,14 @@
+username='SaraFallah'
+password=99254067
+counter=0
+while (counter<3):
+  name=input('please enter username:')
+  pas=int(input('please enter the password:')) 
+  if (name==username) and (pas==password):
+    print('welcom')
+    break
+  else:
+    print('password or username is incorrect')
+  counter=counter+1
+if(counter>2):
+    print('access denied')
